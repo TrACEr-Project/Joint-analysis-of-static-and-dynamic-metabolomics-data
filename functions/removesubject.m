5e19ba80349d4c65c16ee496383e6af4c6e21070
eacarat <evrim.acarataman@gmail.com>
2024-02-01 11:03:56 +0100

    Fix function name

--- functions/removesubject.m
+++ functions/removesubject.m
@@ -3,7 +3,7 @@
 % outlier_pid: the index of the subjects to be removed
 % input Y: the input dataset
 
-function Y=removeoutlier(Y,outlier_pid)
+function Y = removesubject(Y,outlier_pid)
 
 [subject_notoutlier,index] = setdiff(str2num(Y.label{1}), outlier_pid);
 Y=Y(index,:,:);
